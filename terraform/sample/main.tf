f45235881a4f32ca3ccc43128985b6768f28f02a
Chandra Ummadi <ummadi.chandra@gmail.com>
2021-11-08 16:04:37 +0530

    initial versions

--- terraform/sample/main.tf
+++ terraform/sample/main.tf
@@ -7,6 +7,7 @@ resource "aws_spot_instance_request" "aws-spot-request" {
   instance_type = "t2.micro"
   spot_type = "persistent"
   vpc_security_group_ids = ["sg-08797573be56216ce"]
+  wait_for_fulfillment = true
 
   tags = {
     Name = "first-instance"
